3dab241b7d8171d8bac8549aaade0ca272f93ddc
Ganesh <ganeshmk247@gmail.com>
2024-04-24 23:57:43 +0530

    Entrance page design

--- frontend/src/pages/EntrancePage.jsx
+++ frontend/src/pages/EntrancePage.jsx
@@ -2,15 +2,23 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import '../App.css'
 import '../style/EntrancePage.scss'
-
 import { images } from '../assets/images.js'
+
 function EntrancePage() {
   return (
     <div className="entranceContainer">
-      <img src={images.codeBroLogo} alt="CodeBro Logo" />
+      <div className="logoContainer">
+        <span className="logo">&lt;&nbsp;</span>
+        <span className="logo code">Code</span>
+        <span className="logo">Bro</span>
+        <span className="logo">&nbsp;&frasl;&gt;</span>
+      </div>
       <div className="container">
         <div className="left">
-          <h2>Start your coding journey today with "CodeBro"</h2>
+          <h2>
+            Start your coding journey today with "
+            <span style={{ color: 'rgb(0, 131, 231)' }}>Code</span>Bro"
+          </h2>
           <div>
             <Link to="/register" className="button">
               Create Account
