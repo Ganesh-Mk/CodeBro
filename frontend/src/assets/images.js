c7154a206f0159eeb58e6e97fe8bbaf15aad187b
Tej <tejhagargigit@gmail.com>
2024-04-26 14:16:29 +0530

    main page modified

--- frontend/src/assets/images.js
+++ frontend/src/assets/images.js
@@ -1,9 +1,15 @@
 import codeBroLogo from './images/codeBroLogo.png'
 import accDefaultLogo from './images/accDefaultLogo.png'
 import entranceImage from './images/entranceImage.png'
+import random from './images/random.png'
+import searchicon from './images/search-icon.png'
+
 
 export const images = {
   codeBroLogo,
   accDefaultLogo,
   entranceImage,
+  random,
+  searchicon,
+
 }
