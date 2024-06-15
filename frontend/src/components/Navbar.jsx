cbe8c3ba0fdd20fd5f62530463621a1a57bf12c0
Ganesh <ganeshmk247@gmail.com>
2024-06-15 17:49:16 +0530

    navbar design done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,81 +1,92 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import '../style/Navbar.scss';
-import { images } from '../javascripts/images';
-import CodeBroLogo from './CodeBroLogo';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "../style/Navbar.scss";
+import { images } from "../javascripts/images";
+import CodeBroLogo from "./CodeBroLogo";
+import axios from "axios";
 
-function Navbar({ fontColor = 'white' }) {
-  const [userImage, setUserImage] = useState('home');
+function Navbar({ fontColor = "white" }) {
+  const [userImage, setUserImage] = useState("home");
   const [menuVisible, setMenuVisible] = useState(false);
   const location = useLocation(); // Get current location
-  
+
   const handleMenuToggle = () => {
-    console.log('menuVisible', menuVisible);
+    console.log("menuVisible", menuVisible);
     setMenuVisible(!menuVisible); // Toggle the menu visibility state
   };
 
   useEffect(() => {
     axios
-      .get('http://localhost:3000/fetchUserImage', {
+      .get("http://localhost:3000/fetchUserImage", {
         params: {
-          userEmail: localStorage.getItem('email'),
+          userEmail: localStorage.getItem("email"),
         },
       })
       .then((response) => {
         setUserImage(response.data.userImage);
       })
       .catch((error) => {
-        console.error('Error fetching user data:', error);
+        console.error("Error fetching user data:", error);
       });
   }, []);
 
-
   return (
     <div className="navbar">
-      <Link to="/aboutus" className="navItems" style={{ textDecoration: 'none' }}>
+      <Link
+        to="/aboutus"
+        className="navItems"
+        style={{ textDecoration: "none" }}
+      >
         <CodeBroLogo />
       </Link>
-      
+
       <div className="navCenter">
         <Link
           to="/home"
-          className={`navItems ${location.pathname === '/home' ? 'active' : ''}`}
-          style={{ textDecoration: 'none', color: fontColor }}
+          className={`navItems ${
+            location.pathname === "/home" ? "active" : ""
+          }`}
+          style={{ textDecoration: "none", color: fontColor }}
         >
           <p>Home</p>
         </Link>
         <Link
           to="/problems"
-          className={`navItems ${location.pathname === '/problems' ? 'active' : ''}`}
-          style={{ textDecoration: 'none', color: fontColor }}
+          className={`navItems ${
+            location.pathname === "/problems" ? "active" : ""
+          }`}
+          style={{ textDecoration: "none", color: fontColor }}
         >
           <p>Problems</p>
         </Link>
         <Link
           to="/programming"
-          className={`navItems ${location.pathname === '/programming' ? 'active' : ''}`}
-          style={{ textDecoration: 'none', color: fontColor }}
+          className={`navItems ${
+            location.pathname === "/programming" ? "active" : ""
+          }`}
+          style={{ textDecoration: "none", color: fontColor }}
         >
           <p>Programming</p>
         </Link>
         <Link
           to="/leaderboard"
-          className={`navItems ${location.pathname === '/leaderboard' ? 'active' : ''}`}
-          style={{ textDecoration: 'none', color: fontColor }}
+          className={`navItems ${
+            location.pathname === "/leaderboard" ? "active" : ""
+          }`}
+          style={{ textDecoration: "none", color: fontColor }}
         >
           <p>Leaderboard</p>
         </Link>
       </div>
 
       <div>
-        <Link to="/account" className="navAccContainer">
+        <Link to="/account" className="navAccContainer navAccContainerNone">
           <img
             className="accLogo"
             style={{
-              width: '2vw',
-              height: '2vw',
-              borderRadius: '100vw',
+              width: "2vw",
+              height: "2vw",
+              borderRadius: "100vw",
             }}
             src={
               userImage
@@ -84,86 +95,81 @@ function Navbar({ fontColor = 'white' }) {
             }
             alt="account default logo"
           />
-          <p style={{ textDecoration: 'none', color: fontColor }}>Account</p>
+          <p
+            className="accLogoText"
+            style={{ textDecoration: "none", color: fontColor }}
+          >
+            Account
+          </p>
         </Link>
       </div>
 
-      <div className="menuIcon" onClick={handleMenuToggle}>
-      {
-        menuVisible ? <span class="material-symbols-outlined">
-          close
-        </span> : <span class="material-symbols-outlined">
-menu
-</span>
-      }
-        
+      <div className="rightMenuMini">
+        <Link to="/account" className="navAccMini">
+          <img
+            className="accLogoMini"
+            src={
+              userImage
+                ? `http://localhost:3000/uploads/${userImage}`
+                : images.accDefaultLogo
+            }
+            alt="account logo"
+          />
+        </Link>
+        <div className="menuIcon" onClick={handleMenuToggle}>
+          {menuVisible ? (
+            <span className="material-symbols-outlined">close</span>
+          ) : (
+            <span className="material-symbols-outlined">menu</span>
+          )}
+        </div>
       </div>
-      <div className={`menuContainer ${menuVisible ? 'visible' : ''}`}>
-      <Link
+
+      <div className={`menuContainer ${menuVisible ? "visible" : ""}`}>
+        <Link
           to="/home"
-          className={`navItems ${location.pathname === '/home' ? 'active' : ''}`}
-          style={{ textDecoration: 'none', color: fontColor }}
+          className={`navItems ${
+            location.pathname === "/home" ? "active" : ""
+          }`}
+          style={{ textDecoration: "none", color: fontColor }}
         >
           <p>Home</p>
         </Link>
         <Link
           to="/problems"
-          className={`navItems ${location.pathname === '/problems' ? 'active' : ''}`}
-          style={{ textDecoration: 'none', color: fontColor }}
+          className={`navItems ${
+            location.pathname === "/problems" ? "active" : ""
+          }`}
+          style={{ textDecoration: "none", color: fontColor }}
         >
           <p>Problems</p>
         </Link>
         <Link
           to="/programming"
-          className={`navItems ${location.pathname === '/programming' ? 'active' : ''}`}
-          style={{ textDecoration: 'none', color: fontColor }}
+          className={`navItems ${
+            location.pathname === "/programming" ? "active" : ""
+          }`}
+          style={{ textDecoration: "none", color: fontColor }}
         >
           <p>Programming</p>
         </Link>
         <Link
           to="/leaderboard"
-          className={`navItems ${location.pathname === '/leaderboard' ? 'active' : ''}`}
-          style={{ textDecoration: 'none', color: fontColor }}
+          className={`navItems ${
+            location.pathname === "/leaderboard" ? "active" : ""
+          }`}
+          style={{ textDecoration: "none", color: fontColor }}
         >
           <p>Leaderboard</p>
         </Link>
-        <div className='menuBottom'>
-        <Link to="/account" className="navAccContainer">
-          <img
-            className="accLogo"
-            style={{
-              width: '2vw',
-              height: '2vw',
-              borderRadius: '100vw',
-            }}
-            src={
-              userImage
-                ? `http://localhost:3000/uploads/${userImage}`
-                : images.accDefaultLogo
-            }
-            alt="account default logo"
-          />
-          <p style={{ textDecoration: 'none', color: fontColor }}>Account</p>
-        </Link>
-        <Link to="/account" className="navAccContainer">
-          <img
-            className="accLogo"
-            style={{
-              width: '2vw',
-              height: '2vw',
-              borderRadius: '100vw',
-            }}
-            src={
-              userImage
-                ? `http://localhost:3000/uploads/${userImage}`
-                : images.accDefaultLogo
-            }
-            alt="account default logo"
-          />
-          <p style={{ textDecoration: 'none', color: fontColor }}>Account</p>
-        </Link>
+        <div className="menuBottom">
+          <Link to="/account" className="navAccContainer">
+            <p style={{ textDecoration: "none", color: fontColor }}>Account</p>
+          </Link>
+          <Link to="/aboutus" className="navAccContainer">
+            <p style={{ textDecoration: "none", color: fontColor }}>About-Us</p>
+          </Link>
         </div>
-       
       </div>
     </div>
   );
