0194ca1a69a0e83cd93823a3f5dadf75f4952e33
Ganesh <ganeshmk247@gmail.com>
2024-06-17 20:51:49 +0530

    leaderboard design starter

--- frontend/src/components/LeaderBoardUsers.jsx
+++ frontend/src/components/LeaderBoardUsers.jsx
@@ -1,53 +1,47 @@
-import React from 'react'
+import React from "react";
 import {
   AccordionButton,
   AccordionIcon,
   AccordionItem,
   AccordionPanel,
-} from '@chakra-ui/react'
-import { images } from '../javascripts/images'
-import { Link } from 'react-router-dom'
+} from "@chakra-ui/react";
+import { images } from "../javascripts/images";
+import { Link } from "react-router-dom";
+import "../style/Leaderboard.css";
 
 function LeaderBoardUsers({
-  rank = '',
-  name = '',
+  rank = "",
+  name = "",
   total = 0,
   easy = 0,
   medium = 0,
   hard = 0,
-  userInsta = 'fdsdf',
-  userEmail = 'sdf',
-  userGithub = 'df',
-  userLinkedin = 'f',
+  userInsta = "fdsdf",
+  userEmail = "sdf",
+  userGithub = "df",
+  userLinkedin = "f",
   javascript = 0,
   python = 0,
   java = 0,
-  image = '',
+  image = "",
 }) {
   return (
     <>
       <AccordionItem>
         <h2>
           <AccordionButton
-            style={{ display: 'flex', justifyContent: 'space-between' }}
+            style={{ display: "flex", justifyContent: "space-between" }}
           >
             <div
               style={{
-                display: 'grid',
-                width: '100%',
-                gridTemplateColumns: '5% 7% 40% 10% 10% 10% 10% 10%',
+                display: "grid",
+                width: "100%",
+                gridTemplateColumns: "5% 7% 40% 10% 10% 10% 10% 10%",
               }}
             >
               <p>{rank}</p>
               <img
-                style={{
-                  textAlign: 'right',
-                  width: '2.7vw',
-                  height: '2.7vw',
-                  borderRadius: '100vw',
-                  marginBottom: '1.2vw',
-                  marginLeft: '3.4vw',
-                }}
+                className="leadersImage"
                 src={
                   image
                     ? `http://localhost:3000/uploads/${image}`
@@ -55,13 +49,13 @@ function LeaderBoardUsers({
                 }
                 alt="account default logo"
               />
-              <p style={{ textAlign: 'left', marginLeft: '2vw' }}>{name}</p>
+              <p style={{ textAlign: "left", marginLeft: "2vw" }}>{name}</p>
               <p>{total}</p>
               <p>{easy}</p>
               <p>{medium}</p>
               <p>{hard}</p>
               <AccordionIcon
-                style={{ textAlign: 'right', marginLeft: '2vw' }}
+                style={{ textAlign: "right", marginLeft: "2vw" }}
               />
             </div>
           </AccordionButton>
@@ -69,73 +63,73 @@ function LeaderBoardUsers({
         <AccordionPanel pb={3} w="100%">
           <div
             style={{
-              display: 'flex',
-              padding: '1vw',
-              justifyContent: 'space-between',
-              gap: '2vw',
+              display: "flex",
+              padding: "1vw",
+              justifyContent: "space-between",
+              gap: "2vw",
             }}
           >
-            <div style={{ display: 'flex', gap: '2vw' }}>
+            <div style={{ display: "flex", gap: "2vw" }}>
               <h1>Connect with - </h1>
-              {userInsta != '' ? (
+              {userInsta != "" ? (
                 <Link className="links" target="_blank" to={userInsta}>
                   <img
-                    style={{ width: '3vw' }}
+                    style={{ width: "3vw" }}
                     src={images.instagram}
                     alt="insta logo"
                   />
                 </Link>
               ) : (
-                ''
+                ""
               )}
-              {userGithub != '' ? (
+              {userGithub != "" ? (
                 <Link className="links" target="_blank" to={userGithub}>
                   <img
-                    style={{ width: '3vw' }}
+                    style={{ width: "3vw" }}
                     src={images.github}
                     alt="Github logo"
                   />
                 </Link>
               ) : (
-                ''
+                ""
               )}
 
-              {userLinkedin != '' ? (
+              {userLinkedin != "" ? (
                 <Link className="links" target="_blank" to={userLinkedin}>
                   <img
-                    style={{ width: '3vw' }}
+                    style={{ width: "3vw" }}
                     src={images.linkedin}
                     alt="LinkedIn logo"
                   />
                 </Link>
               ) : (
-                ''
+                ""
               )}
 
-              {userEmail != '' ? (
+              {userEmail != "" ? (
                 <Link className="links" target="_blank" to={userEmail}>
                   <img
-                    style={{ width: '3vw' }}
+                    style={{ width: "3vw" }}
                     src={images.mail}
                     alt="Mail logo"
                   />
                 </Link>
               ) : (
-                ''
+                ""
               )}
             </div>
-            <div style={{ display: 'flex', gap: '2vw' }}>
+            <div style={{ display: "flex", gap: "2vw" }}>
               <h1>Solved in - </h1>
               <p>
-                Javascript <span style={{ color: 'grey' }}>x</span>
+                Javascript <span style={{ color: "grey" }}>x</span>
                 {javascript}
               </p>
               <p>
-                Python <span style={{ color: 'grey' }}>x</span>
+                Python <span style={{ color: "grey" }}>x</span>
                 {python}
               </p>
               <p>
-                java <span style={{ color: 'grey' }}>x</span>
+                java <span style={{ color: "grey" }}>x</span>
                 {java}
               </p>
             </div>
@@ -143,7 +137,7 @@ function LeaderBoardUsers({
         </AccordionPanel>
       </AccordionItem>
     </>
-  )
+  );
 }
 
-export default LeaderBoardUsers
+export default LeaderBoardUsers;
--- frontend/src/pages/LeaderboardPage.jsx
+++ frontend/src/pages/LeaderboardPage.jsx
@@ -1,134 +1,128 @@
-import React, { useEffect } from 'react'
-import { Accordion } from '@chakra-ui/react'
-import Navbar from '../components/Navbar'
-import LeaderBoardUsers from '../components/LeaderBoardUsers'
-import { useSelector, useDispatch } from 'react-redux'
-import { setLeaderBoardEntries } from '../store/leaderBoardSlice'
-import axios from 'axios'
+import React, { useEffect } from "react";
+import { Accordion } from "@chakra-ui/react";
+import Navbar from "../components/Navbar";
+import LeaderBoardUsers from "../components/LeaderBoardUsers";
+import { useSelector, useDispatch } from "react-redux";
+import { setLeaderBoardEntries } from "../store/leaderBoardSlice";
+import axios from "axios";
+import "../style/Leaderboard.css";
 
 function LeaderBoardPage() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const leaderBoardEntries = useSelector(
-    (state) => state.leaderBoard.leaderBoardEntries,
-  )
+    (state) => state.leaderBoard.leaderBoardEntries
+  );
 
   useEffect(() => {
     async function fetchLeaderBoard() {
       await axios
-        .get('http://localhost:3000/leaderBoardprint')
+        .get("http://localhost:3000/leaderBoardprint")
         .then((response) => {
-          dispatch(setLeaderBoardEntries(response.data))
+          dispatch(setLeaderBoardEntries(response.data));
         })
         .catch((error) => {
-          console.error('Error LeaderBoard problem record:', error)
-        })
+          console.error("Error LeaderBoard problem record:", error);
+        });
     }
-    fetchLeaderBoard()
-  }, [])
+    fetchLeaderBoard();
+  }, []);
 
   let sortedEntries = [...leaderBoardEntries].sort((a, b) => {
     if (b.total !== a.total) {
-      return b.total - a.total
+      return b.total - a.total;
     }
     if (b.hard !== a.hard) {
-      return b.hard - a.hard
+      return b.hard - a.hard;
     }
     if (b.medium !== a.medium) {
-      return b.medium - a.medium
+      return b.medium - a.medium;
     }
-    return 0
-  })
+    return 0;
+  });
 
   useEffect(() => {
-    let email = localStorage.getItem('email')
+    let email = localStorage.getItem("email");
     if (sortedEntries.length > 0) {
       sortedEntries.forEach((entry, i) => {
         if (entry.email === email) {
-          localStorage.setItem('rank', i + 1)
+          localStorage.setItem("rank", i + 1);
         }
-      })
+      });
     }
-  }, [sortedEntries])
+  }, [sortedEntries]);
 
   return (
     <>
       <Navbar />
-      <div className="leaderBoardPage" style={{ padding: '0vw 5vw 10vw 5vw' }}>
-        <h3
-          style={{
-            fontSize: '3vw',
-            margin: '4vw 0',
-            color: 'white',
-            textAlign: 'center',
-          }}
-        >
-          Leader board
-          <p style={{ fontSize: '1vw', color: 'grey' }}>
-            Highest Problems Solved Users
-          </p>
-        </h3>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            marginBottom: '2vw',
-          }}
-        >
+      <div className="leaderBoardPage" style={{ padding: "0vw 5vw 10vw 5vw" }}>
+        <h3>Leader board</h3>
+        <p>Highest Problems Solved Users</p>
+        <div className="leaderBottom">
           <div
             style={{
-              display: 'grid',
-              width: '100%',
-              gridTemplateColumns: '5% 7% 40% 10% 10% 10% 10% 10%',
-              margin: '0 2vw',
+              display: "flex",
+              justifyContent: "space-between",
+              marginBottom: "2vw",
             }}
           >
-            <p>Rank</p>
-            <p style={{ textAlign: 'right', marginLeft: '2vw' }}></p>
-            <p style={{ textAlign: 'left', marginLeft: '2vw' }}>Name</p>
-            <p>Total</p>
-            <p>Easy</p>
-            <p>Medium</p>
-            <p>Hard</p>
-            <p>More</p>
-          </div>
-        </div>
-        <Accordion allowToggle>
-          {sortedEntries.length === 0 ? (
-            <p
+            <div
               style={{
-                fontSize: '1.5vw',
-                color: 'grey',
-                textAlign: 'center',
-                marginTop: '10vw',
+                display: "grid",
+                width: "100%",
+                gridTemplateColumns: "5% 7% 40% 10% 10% 10% 10% 10%",
+                margin: "0 2vw",
               }}
             >
-              Be the first one in leaderboard
-            </p>
-          ) : (
-            sortedEntries.map((user, i) => (
-              <LeaderBoardUsers
-                key={i}
-                rank={i + 1}
-                image={user.image}
-                name={user.name}
-                total={user.total}
-                easy={user.easy}
-                medium={user.medium}
-                hard={user.hard}
-                userInsta={user.insta}
-                userEmail={user.email}
-                userGithub={user.github}
-                userLinkedin={user.linkedin}
-                javascript={user.javascript}
-                python={user.python}
-                java={user.java}
-              />
-            ))
-          )}
-        </Accordion>
+              <p>Rank</p>
+              <p style={{ textAlign: "right", marginLeft: "2vw" }}></p>
+              <p style={{ textAlign: "left", marginLeft: "2vw" }}>Name</p>
+              <p>Total</p>
+              <p>Easy</p>
+              <p>Medium</p>
+              <p>Hard</p>
+              <p>More</p>
+            </div>
+          </div>
+          <div className="leadersBox">
+            <Accordion allowToggle>
+              {sortedEntries.length === 0 ? (
+                <p
+                  style={{
+                    fontSize: "1.5vw",
+                    color: "grey",
+                    textAlign: "center",
+                    marginTop: "10vw",
+                  }}
+                >
+                  Be the first one in leaderboard
+                </p>
+              ) : (
+                sortedEntries.map((user, i) => (
+                  <LeaderBoardUsers
+                    key={i}
+                    rank={i + 1}
+                    image={user.image}
+                    name={user.name}
+                    total={user.total}
+                    easy={user.easy}
+                    medium={user.medium}
+                    hard={user.hard}
+                    userInsta={user.insta}
+                    userEmail={user.email}
+                    userGithub={user.github}
+                    userLinkedin={user.linkedin}
+                    javascript={user.javascript}
+                    python={user.python}
+                    java={user.java}
+                  />
+                ))
+              )}
+            </Accordion>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default LeaderBoardPage
+export default LeaderBoardPage;
