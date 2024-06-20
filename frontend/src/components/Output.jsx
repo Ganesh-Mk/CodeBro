8c937913615873a92ab871c892707058c862dbd1
Ganesh <ganeshmk247@gmail.com>
2024-06-20 19:17:13 +0530

    ui changes

--- frontend/src/components/Output.jsx
+++ frontend/src/components/Output.jsx
@@ -6,30 +6,30 @@ import {
   TabPanels,
   Tabs,
   CircularProgress,
-} from '@chakra-ui/react'
-import TestCase from './TestCase'
-import { useSelector } from 'react-redux'
-import { useState } from 'react'
-import CorrectIcon from './CorrectIcon'
-import WrongIcon from './WrongIcon'
-import '../style/Output.scss'
+} from "@chakra-ui/react";
+import TestCase from "./TestCase";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import CorrectIcon from "./CorrectIcon";
+import WrongIcon from "./WrongIcon";
+import "../style/Output.scss";
 const Output = ({ isError }) => {
-  const problemObj = useSelector((state) => state.problemObj.obj)
+  const problemObj = useSelector((state) => state.problemObj.obj);
 
   return (
     <>
       <Tabs className="outputBox" size="md" variant="soft-rounded" isFitted>
-        <TabList style={{ margin: '1vw 0' }}>
+        <TabList style={{ margin: "1vw 0" }}>
           {problemObj.example.length > 0
             ? problemObj.example.map((testCase, i) => (
                 <Tab
                   className="outputCaseTab"
                   key={i}
-                  style={{ margin: '0 1vw' }}
-                  _selected={{ backgroundColor: '#353535' }}
-                  _hover={{ backgroundColor: '#353535' }}
+                  style={{ margin: "0 1vw" }}
+                  _selected={{ backgroundColor: "#353535" }}
+                  _hover={{ backgroundColor: "#353535" }}
                 >
-                  <p style={{ marginRight: '2vw' }}>Case {i + 1}</p>
+                  <p style={{ marginRight: "2vw" }}>Case {i + 1}</p>
                   {problemObj.allResult.length > 0 ? (
                     <p>
                       {problemObj.allResult[i] === true ? (
@@ -40,17 +40,18 @@ const Output = ({ isError }) => {
                         <CircularProgress
                           isIndeterminate
                           thickness={15}
-                          size="2vw"
+                          size="1.3rem"
+                          className="icon"
                           color="green.300"
                         />
                       )}
                     </p>
                   ) : (
-                    ''
+                    ""
                   )}
                 </Tab>
               ))
-            : ''}
+            : ""}
         </TabList>
         <TabPanels className="outputContainer">
           {problemObj.example.map((testCase, i) => (
@@ -66,6 +67,6 @@ const Output = ({ isError }) => {
         </TabPanels>
       </Tabs>
     </>
-  )
-}
-export default Output
+  );
+};
+export default Output;
--- frontend/src/components/WrongIcon.jsx
+++ frontend/src/components/WrongIcon.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import wrongImage from '../assets/icons/wrongIcon.png'
-import '../style/style.css'
+import React from "react";
+import wrongImage from "../assets/icons/wrongIcon.png";
+import "../style/style.css";
 
 function WrongIcon() {
   return (
     <div>
       <img className="icons" src={wrongImage} alt="correctImg" />
     </div>
-  )
+  );
 }
 
-export default WrongIcon
+export default WrongIcon;
